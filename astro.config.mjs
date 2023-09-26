5051df61843b2062a82b6bbc3efdfbf1d4fe6676
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-09-26 22:28:01 +0100

    Add vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
   integrations: [react()],
+  adapter: vercel(),
 });
