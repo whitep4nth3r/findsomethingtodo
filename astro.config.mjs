be180afe0005d53e7950fba78de52706069838b7
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-09-26 19:51:39 +0100

    Fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  dapter: netlify(),
+  adapter: netlify(),
   integrations: [react()],
 });
