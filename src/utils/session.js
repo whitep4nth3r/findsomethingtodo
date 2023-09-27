67d79662c79011f1ad0e34b3b9d8e76982096adb
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-09-27 13:42:20 +0100

    Abstract clerk to utils file

---
+++ src/utils/session.js
@@ -0,0 +1,14 @@
+import Clerk from "@clerk/clerk-js";
+
+const CLERK_FRONTEND_API = "pk_test_dW5iaWFzZWQtY2hpY2tlbi03NS5jbGVyay5hY2NvdW50cy5kZXYk";
+
+export async function getSession() {
+  const clerk = new Clerk(CLERK_FRONTEND_API);
+  await clerk.load({});
+
+  return clerk.session;
+}
+
+export function initClerk() {
+  return new Clerk(CLERK_FRONTEND_API);
+}
