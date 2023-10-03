11de9fae8c39db6eb204186081ab1fdb8ea5d8a5
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-10-03 08:49:27 +0100

    Try netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
-  adapter: vercel(),
+  adapter: netlify()
 });
