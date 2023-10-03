9ed67ed8f59ded854479c6461d53a93292746281
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-10-03 09:24:36 +0100

    Add Netlify toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
