3d4f208c8f57b1f7f76a8b726ed40a8e9aa71e40
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-10-10 10:19:13 +0100

    Sort todos by created desc

--- src/pages/api/get-todos.json.js
+++ src/pages/api/get-todos.json.js
@@ -11,7 +11,7 @@ async function initDatabase() {
 export async function GET({ params, request }) {
   const mongo = await initDatabase();
   const todos = mongo.db("findsomethingtodo").collection("todos");
-  const todosNotFlagged = await todos.find({ flagged: false }).toArray();
+  const todosNotFlagged = await todos.find({ flagged: false }).sort({ _id: -1 }).toArray();
 
   return new Response(
     JSON.stringify({
